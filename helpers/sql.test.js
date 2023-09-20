4b575faba3bd2426ddd97f6e1a58a43e5c7934e7
Megan Vyenielo <mvyenielo58@gmail.com>
2023-09-20 11:50:04 -0700

    added schema for filtering company search, finished tests for SQL partial update function

--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,25 +1,47 @@
-"use strict"
+"use strict";
 
+const { BadRequestError } = require('../expressError');
 // const request = require("supertest");
 const { sqlForPartialUpdate } = require('./sql');
 
-describe("sqlForPartialUpdate", function() {
-  it("updates the correct column when given one column to update",
-      function() {
-        const data = { numEmployees: 796 };
-        const jsToSql = {
-          numEmployees: "num_employees",
-          logoUrl: "logo_url"
-        };
+describe("sqlForPartialUpdate", function () {
+  it(`Returns the correct column string and value
+      array when given one column to update`,
+    function () {
+      const data = { count: 796 };
+      const jsToSql = {};
 
-        const result = sqlForPartialUpdate(data, jsToSql);
+      const result = sqlForPartialUpdate(data, jsToSql);
 
-        expect(result).toEqual({
-          setCols: '"num_employees"=$1',
-          values: [ 796 ]
-          }
-        )
-  });
+      expect(result).toEqual({
+        setCols: '"count"=$1',
+        values: [796]
+      }
+      );
 
-  // it("Throws correct error when no columns provided")
-})
+    });
+
+  it("Throws correct error when no columns provided",
+    function () {
+      const data = {};
+      const jsToSql = {
+        numEmployees: "num_employees",
+        logoUrl: "logo_url"
+      };
+
+      expect(() => sqlForPartialUpdate(data, jsToSql)).toThrow(BadRequestError);
+    });
+
+  it("Formats column names correctly, camelCase => snake_case",
+    function () {
+      const data = { numEmployees: 796 };
+      const jsToSql = {
+        numEmployees: "num_employees",
+      };
+
+      const result = sqlForPartialUpdate(data, jsToSql);
+
+      expect(result.setCols).toContain("num_employees");
+
+    });
+});
