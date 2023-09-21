cf6028fbfdd033f0ea436e73a0cb701aea74a7ab
Daniel Zych <danjzych@gmail.com>
2023-09-21 17:58:52 -0500

    Finished tests for Job model

--- models/job.test.js
+++ models/job.test.js
@@ -1,7 +1,19 @@
 "use strict";
 
 const db = require('../db');
+const { NotFoundError } = require('../expressError');
 const Job = require('./job');
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
 
 /************************************** create */
 
@@ -118,10 +130,139 @@ describe("findAll", function () {
 });
 
 /************************************** sqlForWhereClause */
+
 describe("sqlForWhereClause", function () {
   test("Returns correct WHERE clause and value array", function () {
     const filterParams = { title: "j2", hasEquity: true };
 
     const result = Job.sqlForWhereClause(filterParams);
+
+    expect(result).toEqual({
+      whereClause: "WHERE title ILIKE $1 AND equity > 0",
+      values: ["%j2%"]
+    });
   });
+
+  test(`Returns object with whereClause key set to empty string and values key set
+  to empty array `,
+    function () {
+      const filterParams = {};
+
+      const result = Job.sqlForWhereClause(filterParams);
+
+      expect(result).toEqual({
+        whereClause: "",
+        values: []
+      });
+
+    });
+});
+
+/************************************** get */
+
+describe("get", function () {
+  test("works with id", async function () {
+    const job = await Job.get(1);
+
+    expect(job).toEqual({
+      id: 1,
+      title: 'J1',
+      salary: 60000,
+      equity: null,
+      company_handle: 'c1'
+    });
+  });
+
+  test("not found error if no such job", async function () {
+    await expect(async () =>
+      await Job.get(100)).rejects.toThrow(NotFoundError);
+  });
+});
+
+/************************************** update */
+
+describe("update", function () {
+  const updateData = {
+    title: 'newTitle',
+    salary: 100000,
+    equity: .005
+  };
+
+  test("works with valid data", async function () {
+    const job = await Job.update(1, updateData);
+
+    expect(job).toEqual({
+      id: 1,
+      ...updateData,
+      company_handle: 'c1'
+    });
+
+    const result = await db.query(`
+    SELECT id, title, salary, equity, company_handle
+      FROM jobs
+      WHERE id = 1`);
+
+    expect(result.rows[0]).toEqual({
+      id: 1,
+      ...updateData,
+      company_handle: 'c1'
+    });
+  });
+
+  test("works: null fields", async function () {
+    const updateDataSetNulls = {
+      title: 'newTitle',
+      salary: null,
+      equity: null
+    };
+
+    const job = await Job.update(1, updateDataSetNulls);
+
+    expect(job).toEqual({
+      id: 1,
+      ...updateDataSetNulls
+    });
+
+    const result = await db.query(`
+    SELECT id, title, salary, equity, company_handle
+      FROM jobs
+      WHERE id = 1`);
+
+    expect(result.rows[0]).toEqual({
+      id: 1,
+      ...updateDataSetNulls,
+      company_handle: 'c1'
+    });
+  });
+
+  test("not found if no such job", async function () {
+    await expect(async () =>
+      await Job.update(100, updateData)).rejects.toThrow(NotFoundError);
+  });
+
+  test("bad request with no data", async function () {
+    await expect(async () =>
+      await Job.update(1, {})), rehects.toThrow(NotFoundError);
+  });
+});
+
+/************************************** remove */
+
+describe("remove", function () {
+  test("works", async function () {
+    const removedJob = await Job.remove(1);
+
+    expect(removedJob).toEqual({ id: 1 });
+
+    const result = await db.query(`
+      SELECT id FROM jobs WHERE id = 1;
+    `);
+
+    expect(result.rows.length).toEqual(0);
+  });
+
+  test("not found error if no such job", async function () {
+    await expect(async () =>
+      await Job.remove(1)).rejects.toThrow(NotFoundError);
+  })
 });
